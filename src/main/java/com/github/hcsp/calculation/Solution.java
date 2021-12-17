208b21681f7fee07b1ee13df7758be3c2608a0e0
wushaolin <95512871+wushao666@users.noreply.github.com>
2021-12-17 23:26:38 +0800

    求解一元二次方程 (#264)
    求解一元二次方程 (#264)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,20 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        //a不能等于0
+        //1、判别式判断跟的个数
+        int discriminant = b * b - 4 * a * c;
+        double[] rootArray = new double[]{};
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            rootArray = new double[]{root1, root2};
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            rootArray = new double[]{root};
+        }
+
+        return rootArray;
+    }
 }
